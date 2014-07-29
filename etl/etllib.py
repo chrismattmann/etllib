7e52d527e7ea24f4e0d7451c0f2a24065379f46d
Michael Joyce <mltjoyce@gmail.com>
2014-07-29 12:23:26 -0700

    Add multidoc preparation support

--- etl/etllib.py
+++ etl/etllib.py
@@ -120,6 +120,18 @@ def prepareDocForSolr(jsondata, unmarshall=True):
     jsonwrapper = {"add": {"boost": boost, "doc": jsondoc}}
     return json.dumps(jsonwrapper)
 
+def prepareDocsForSolr(jsondata, unmarshall=True):
+    jsondocs = json.loads(jsondata) if unmarshall else jsondata
+    docs = []
+    for doc in jsondocs:
+        if "boost" in doc:
+            boost = doc["boost"]
+        else:
+            boost = 1.0
+        docs.append({"add": {"boost": boost, "doc": doc}})
+
+    return json.dumps(docs)
+
 def _createOrAppendToList(doc, key, val):
     if key in doc:
         doc[key].append(val)
